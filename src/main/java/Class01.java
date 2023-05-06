3b0a048a312de1ed5a236b81cd7370bb980d7164
ferhat <travaci@gmail.com>
2023-05-06 21:46:14 +0300

    Ver3

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -3,5 +3,6 @@ public class Class01 {
     public static void main(String[] args) {
         //V1 calismalarim...
         //V2 calismalarim...
+        System.out.println("v3 kodlari..."); //V3
     }
 }
