7b82c81e6e7650adb2eaf0d033a8fce8b8c4e2c2
travaci <travaci@gmail.com>
2023-05-06 22:41:33 +0300

    Update Class01.java

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -4,7 +4,6 @@ public class Class01 {
         //V1 calismalarim...
         //V2 calismalarim...
         System.out.println("v3 kodlari..."); //V3
-        //V4 kisa yoldan commit
-        //Github changes
+      
     }
 }
